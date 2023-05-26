278fab865739e1bb14027e0f6680f2153c2c0ce9
Cristian camilo <cristianmilo07@gmail.com>
2023-05-26 10:06:10 -0500

    una aplicación con html

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Home page')
+app.use(express.static('public'));
+
+app.get('/generic', function (req, res) {
+    res.sendFile(__dirname + '/public/generic.html')
 })
 
-app.get('/hola-mundo', function (req, res) {
-    res.send('hola mundo en su respectiva ruta')
-  })
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html')
+})
 
 app.get('*', function (req, res) {
-    res.send('404 | page not found')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.listen(3000)
