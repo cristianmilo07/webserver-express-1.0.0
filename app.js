5bac3bb73a06e9e610a47390ae2a3ba17ec497bf
Cristian camilo <cristianmilo07@gmail.com>
2023-05-25 20:13:53 -0500

    explicación

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 8080;
 
-http.createServer(( req, res) => {
-    res.write('hola mundo');
-    res.end();
+app.get('/', function (req, res) {
+  res.send('Home page')
 })
-.listen( 8080 );
 
-console.log('Escuchando el puerto' , 8080)
+app.get('/hola-mundo', function (req, res) {
+    res.send('hola mundo en su respectiva ruta')
+  })
+
+app.get('*', function (req, res) {
+    res.send('404 | page not found')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.listen(3000)
